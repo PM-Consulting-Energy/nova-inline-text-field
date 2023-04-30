ffb26ee06d0d84387bd4ca680f7afbf8b9f7302f
António Azevedo <antonio.v.azevedo@gmail.com>
2023-04-30 19:24:02 +0100

    Update NovaInlineTextFieldController.php

--- src/Http/Controllers/NovaInlineTextFieldController.php
+++ src/Http/Controllers/NovaInlineTextFieldController.php
@@ -4,6 +4,7 @@ namespace Outl1ne\NovaInlineTextField\Http\Controllers;
 
 use Exception;
 use Illuminate\Routing\Controller;
+use Laravel\Nova\Panel;
 use Outl1ne\NovaInlineTextField\InlineText;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -25,19 +26,29 @@ class NovaInlineTextFieldController extends Controller
         // Find field in question
         try {
             $model = $request->model()->find($modelId);
-            $resource = new $resourceClass($model);
 
-            $allFields = collect($resource->fields($request));
-            $field = $allFields->first(function ($field) use ($attribute) {
+            $resource = $request->newResourceWith($model);
+
+            $allFields = collect($resource->fields($request))->map(function($field) {
+                if (get_class($field) === Panel::class) {
+                    return $field->data;
+                }
+
+                return [$field];
+            })->flatten(1);
+
+            $field = collect($allFields)->first(function ($field) use ($attribute) {
                 return get_class($field) === InlineText::class && $field->attribute === $attribute;
             });
 
             $field->fillInto($request, $model, $attribute);
             $model->save();
+
+            $field->resolve($model, $attribute);
         } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
 
-        return response('', 204);
+        return response()->json(['value' => $field->value]);
     }
 }
